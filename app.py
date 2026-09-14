3f22e4b786fb19192d14359ba1b41b5e6c47dab8
jacquelinet49 <jacquelinet49@nycstudents.net>
2026-09-14 07:21:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+
+def message(input):
+    print(input)
+message("Hello Class")
+
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right()
+
+turtle.done()
+
+
+
+
